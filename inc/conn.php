c59dc40d44562556d0e09d119e3b28665dbfd137
Spirit-act <spirit.info@gmx.net>
2017-05-08 21:07:44 +0200

    Create conn.php

---
+++ inc/conn.php
@@ -0,0 +1,6 @@
+<?php
+  $Vserver = TeamSpeak3::factory('serverquery://username:password@127.0.0.1:10011/?server_port=9987&nickname=Bot&no_query_clients=1&blocking=0');
+  // Establish connection with TS3 Server
+  // nickname = Bot
+  // in the TSviewer is no Query Client visible (no_query_clients=1)
+?>
